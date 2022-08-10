7d9190cca6cd79b19ed19e30fb0c8acee0146c05
Aaron Spencer <aaron.spencer@syf.com>
2022-08-10 17:39:26 +0000

    Add s3 module

---
+++ s3-bucket.tf
@@ -0,0 +1,16 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
+
+variable "bucket_prefix" {
+  type = string
+  default = "aaron-spencer"
+}
