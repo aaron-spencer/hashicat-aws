2f3caf750bd39ae98d434d42403c4525673c03b3
Aaron Spencer <aaron.spencer@syf.com>
2022-08-10 19:30:55 +0000

    another s3 try

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,16 +1,10 @@
 module "s3_bucket" {
   source  = "app.terraform.io/aaron-org/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "aaronspencer-my-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
 
-}
-
-variable "bucket_prefix" {
-  type = string
-  default = "aaron-spencer"
-}
